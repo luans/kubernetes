ffe65d0c46090eb693b4b5ae3ca57257fcff23ca
Luan Lopes <luan.lopes94@gmail.com>
2026-01-19 17:53:46 -0300

    feat: adjust istio gateway appointments.

--- terraform/digitalocean/main.tf
+++ terraform/digitalocean/main.tf
@@ -50,45 +50,6 @@ resource "helm_release" "istiod" {
   ]
 }
 
-## Istio Ingress Gateway
-resource "helm_release" "istio_ingress" {
-  name             = "istio-ingress"
-  repository       = "https://istio-release.storage.googleapis.com/charts"
-  chart            = "gateway"
-  namespace        = "istio-ingress"
-  create_namespace = true
-  atomic           = true
-
-  # Depende do Control Plane
-  depends_on = [helm_release.istiod]
-
-  # Configuração explícita das portas (NodePort)
-  values = [
-    <<EOF
-service:
-  type: NodePort
-  selector:
-    istio: ingress
-  ports:
-  - name: status-port
-    port: 15021
-    targetPort: 15021
-    nodePort: 30021   # Porta de status/healthcheck do Istio
-    protocol: TCP
-  - name: http2
-    port: 80
-    targetPort: 80
-    nodePort: 30080   # Porta HTTP fixa
-    protocol: TCP
-  - name: https
-    port: 443
-    targetPort: 443
-    nodePort: 30443   # Porta HTTPS fixa
-    protocol: TCP
-EOF
-  ]
-}
-
 ## Prometheus Community
 resource "helm_release" "prometheus_stack" {
   name       = "prometheus-stack"
@@ -164,44 +125,6 @@ resource "helm_release" "argocd" {
   atomic           = true
   timeout          = 600
 }
-
-## External load balancer (Managed by Terraform)
-resource "digitalocean_loadbalancer" "public_lb" {
-  name   = "kubernetes-lb-development"
-  region = "nyc1"
-
-  droplet_tag = "k8s:${digitalocean_kubernetes_cluster.kubernetes-doks-development.id}"
-
-  forwarding_rule {
-    entry_port      = 80
-    entry_protocol  = "tcp"
-    target_port     = 30080
-    target_protocol = "tcp"
-  }
-
-  forwarding_rule {
-    entry_port      = 443
-    entry_protocol  = "tcp"
-    target_port     = 30443
-    target_protocol = "tcp"
-  }
-
-  healthcheck {
-    port                     = 30021
-    protocol                 = "tcp"
-    check_interval_seconds   = 10
-    response_timeout_seconds = 5
-    unhealthy_threshold      = 3
-    healthy_threshold        = 3
-  }
-}
-
-## Load balancer IP output
-output "load_balancer_ip" {
-  value       = digitalocean_loadbalancer.public_lb.ip
-  description = "Aponte seu domínio (A Record) para este IP"
-}
-
 
 # Install cert-manager
 resource "helm_release" "cert_manager" {
